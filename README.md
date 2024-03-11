7f8de7b3384a8c9a827beab73ff2af2535634dc1
kimkun07 <kimkun07@naver.com>
2024-03-11 13:55:38 +0900

    Backend test locally - select

---
+++ README.md
@@ -0,0 +1,9 @@
+## Install
+1. Install python 3.6
+2. Install virtualenv 20.6.0 (latest version may not work with python 3.6)
+3. Create virtualenv `my_env` with python 3.6\
+  `python -m virtualenv -p C:\Python36\python.exe my_env`\
+  `.\my_env\Scripts\activate`\
+  `pip install -r requirements.txt`
+
+(These steps are tested on Windows 11)
--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from flask import abort, Flask, request, send_from_directory
 import pandas as pd
 import requests
 
-from constant import CHATGPT_SYSTEM_QUERY, CHATGPT_USER_QUERY, CHATGPT_FOOD_QUERY, CHATGPT_ANSWER_QUERY
+from constant import (
+    CHATGPT_SYSTEM_QUERY,
+    CHATGPT_USER_QUERY,
+    CHATGPT_FOOD_QUERY,
+    CHATGPT_ANSWER_QUERY,
+)
 
 data = pd.read_csv("olle.csv")
-data1 = pd.read_csv("description.csv", sep='\\')
+data1 = pd.read_csv("description.csv", sep="\\")
 
 print(data)
 print(data1)
@@ -21,9 +26,7 @@ def serve_image(courseNumber):
 
 @app.route("/api/ready", methods=["GET"])
 def root():
-    return {
-        "message": "hello, world!"
-    }
+    return {"message": "hello, world!"}
 
 
 @app.route("/api/testchatgpt", methods=["GET"])
@@ -41,11 +44,12 @@ def call_testchatgpt():
     description = description.iloc[0]["description"]
     result["description"] = description
 
-    with open('images/' + str(number) + '.png', 'rb') as f:
+    with open("images/" + str(number) + ".png", "rb") as f:
         encoded_image = base64.b64encode(f.read())
     result["image"] = encoded_image
-    
+
     return result.to_json()
+
 
 @app.route("/api/chatgpt", methods=["GET"])
 def call_chatgpt():
@@ -56,9 +60,7 @@ def call_chatgpt():
 
     response = requests.post(
         url="https://api.openai.com/v1/chat/completions",
-        headers={
-            "Authorization": "Bearer AA"
-        },
+        headers={"Authorization": "Bearer AA"},
         json={
             "model": "gpt-4",
             "messages": [
@@ -66,25 +68,21 @@ def call_chatgpt():
                     "role": "system",
                     "content": CHATGPT_SYSTEM_QUERY,
                 },
-                {
-                    "role": "user",
-                    "content": CHATGPT_USER_QUERY.format(theme=theme)
-                },
-
+                {"role": "user", "content": CHATGPT_USER_QUERY.format(theme=theme)},
             ],
             "temperature": 0,
         },
         proxies={
             "http": "http://krmp-proxy.9rum.cc:3128",
-            "https": "http://krmp-proxy.9rum.cc:3128"
-        }
+            "https": "http://krmp-proxy.9rum.cc:3128",
+        },
     )
 
     # response.raise_for_status()
     result = response.json()
     try:
         number = result["choices"][0]["message"]["content"]
-        
+
         result = data[data["올레길 코스 번호"] == (str(number) + "코스")]
         result = result.iloc[0]
 
@@ -92,19 +90,18 @@ def call_chatgpt():
         description = description.iloc[0]["description"]
         result["description"] = description
 
-        with open('images/' + str(number) + '.png', 'rb') as f:
+        with open("images/" + str(number) + ".png", "rb") as f:
             encoded_images = base64.b64encode(f.read())
         result["images"] = encoded_images
 
-        with open('image2/' + str(number) + 'Struc'+'.png', 'rb') as f1:
+        with open("image2/" + str(number) + "Struc" + ".png", "rb") as f1:
             encoded_image2 = base64.b64encode(f1.read())
         result["image2"] = encoded_image2
-
 
         return result.to_json()
     except:
         return "ERROR"
-    
+
     return "ERROR-NOTRY"
 
 
@@ -117,9 +114,7 @@ def call_chatgpt2():
 
     response = requests.post(
         url="https://api.openai.com/v1/chat/completions",
-        headers={
-            "Authorization": "Bearer AA"
-        },
+        headers={"Authorization": "Bearer AA"},
         json={
             "model": "gpt-4",
             "messages": [
@@ -127,36 +122,32 @@ def call_chatgpt2():
                     "role": "system",
                     "content": CHATGPT_FOOD_QUERY,
                 },
-                {
-                    "role": "user",
-                    "content": CHATGPT_ANSWER_QUERY.format(food=food)
-                },
-
+                {"role": "user", "content": CHATGPT_ANSWER_QUERY.format(food=food)},
             ],
             "temperature": 0,
-
         },
         proxies={
             "http": "http://krmp-proxy.9rum.cc:3128",
-            "https": "http://krmp-proxy.9rum.cc:3128"
-        }
+            "https": "http://krmp-proxy.9rum.cc:3128",
+        },
     )
 
     # response.raise_for_status()
     return response.json()
 
+
 # Custom CORS middleware
 @app.after_request
 def add_cors_headers(response):
     # Allow requests from any origin
-    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers["Access-Control-Allow-Origin"] = "*"
     # Allow credentials (cookies, authorization headers, etc.) to be included in the request
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers["Access-Control-Allow-Credentials"] = "true"
     # Specify allowed headers
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     # Specify allowed methods
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-    return response 
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    return response
 
 
 if __name__ == "__main__":
