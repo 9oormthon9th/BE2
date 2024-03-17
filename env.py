184c46194b9d85fc87b8cf3d121bf5bce98d7d2c
kimkun07 <kimkun07@naver.com>
2024-03-17 16:02:05 +0900

    Made secret file

--- env.py
+++ env.py
@@ -3,17 +3,25 @@ from dotenv import load_dotenv
 
 
 class Env:
-    isProduction = "ENV" in os.environ and os.environ["ENV"] == "production"
-    isDevelopment = not isProduction
+    isProduction: bool
+    isDevelopment: bool
+    OPENAI_API_KEY: str
 
     def __init__(self) -> None:
+        self.isProduction = "ENV" in os.environ and os.environ["ENV"] == "production"
+        self.isDevelopment = not self.isProduction
         if self.isProduction:
             load_dotenv(".env.production")
         else:
             load_dotenv(".env.development")
 
+        load_dotenv(".secret")
+        self.OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
     def __str__(self) -> str:
-        return f"isProduction: {self.isProduction}\nisDevelopment: {self.isDevelopment}"
+        return "\n".join(
+            [f"{key}: {value}" for key, value in vars(self).items()]
+        )
 
 
 # exported
