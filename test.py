6caf93a089e10dfac11b03edf6eef82e98672814
kimkun07 <kimkun07@naver.com>
2024-03-18 00:02:10 +0900

    feat: Use assistant API

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
-from openai.recommend import recommend_course
+from openai.api import course_recommend_pipeline
 
-print(recommend_course("산"))
+print(course_recommend_pipeline("가파도의 매력을 느낄 수 있는 올레길"))
+print(course_recommend_pipeline("나무가 많은 올레길"))
+print(course_recommend_pipeline("서귀포에서 걸을 수 있는 올레길"))
