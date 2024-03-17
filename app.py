bf0a10d9bb6573c782d5484bd43a32cb027d0866
kimkun07 <kimkun07@naver.com>
2024-03-17 09:35:59 +0900

    Use /api blueprint. Move csv into data directory

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import base64
-from flask import abort, Flask, request, send_from_directory
+from flask import Blueprint, abort, Flask, request, send_from_directory
 import pandas as pd
 import requests
 
@@ -10,48 +10,53 @@ from constant import (
     CHATGPT_ANSWER_QUERY,
 )
 
-data = pd.read_csv("olle.csv")
-data1 = pd.read_csv("description.csv", sep="\\")
-
+data = pd.read_csv("data/olle.csv")
+data1 = pd.read_csv("data/description.csv", sep="\\")
 print(data)
 print(data1)
 
-app = Flask(__name__)
+api_bp = Blueprint("api", __name__, url_prefix="/api")
 
 
-@app.route("/images/<path:courseNumber>")
+@api_bp.route("/images/<path:courseNumber>")
 def serve_image(courseNumber):
     return send_from_directory("images", courseNumber + ".jpg")
 
 
-@app.route("/api/ready", methods=["GET"])
-def root():
+@api_bp.route("/ready", methods=["GET"])
+def ready():
     return {"message": "hello, world!"}
 
 
-@app.route("/api/testchatgpt", methods=["GET"])
+def course_info(courseNum):
+    result = data[data["올레길 코스 번호"] == (courseNum + "코스")]
+    result = result.iloc[0]
+
+    description = data1[data1["course"] == (courseNum)]
+    description = description.iloc[0]["description"]
+    result["description"] = description
+
+    with open("images/" + courseNum + ".png", "rb") as f:
+        encoded_image = base64.b64encode(f.read())
+        result["image"] = encoded_image
+    with open("image2/" + courseNum + "Struc.png", "rb") as f1:
+        encoded_image2 = base64.b64encode(f1.read())
+        result["image2"] = encoded_image2
+
+    return result.to_json()
+
+
+@api_bp.route("/testchatgpt", methods=["GET"])
 def call_testchatgpt():
     theme = request.args.get("theme")
 
     if not theme:
         abort(400)
 
-    number = 6
-    result = data[data["올레길 코스 번호"] == (str(number) + "코스")]
-    result = result.iloc[0]
-
-    description = data1[data1["course"] == (str(number))]
-    description = description.iloc[0]["description"]
-    result["description"] = description
-
-    with open("images/" + str(number) + ".png", "rb") as f:
-        encoded_image = base64.b64encode(f.read())
-    result["image"] = encoded_image
-
-    return result.to_json()
+    return course_info("6")
 
 
-@app.route("/api/chatgpt", methods=["GET"])
+@api_bp.route("/chatgpt", methods=["GET"])
 def call_chatgpt():
     theme = request.args.get("theme")
 
@@ -81,31 +86,15 @@ def call_chatgpt():
     # response.raise_for_status()
     result = response.json()
     try:
-        number = result["choices"][0]["message"]["content"]
-
-        result = data[data["올레길 코스 번호"] == (str(number) + "코스")]
-        result = result.iloc[0]
-
-        description = data1[data1["course"] == (str(number))]
-        description = description.iloc[0]["description"]
-        result["description"] = description
-
-        with open("images/" + str(number) + ".png", "rb") as f:
-            encoded_images = base64.b64encode(f.read())
-        result["images"] = encoded_images
-
-        with open("image2/" + str(number) + "Struc" + ".png", "rb") as f1:
-            encoded_image2 = base64.b64encode(f1.read())
-        result["image2"] = encoded_image2
-
-        return result.to_json()
+        courseNum = result["choices"][0]["message"]["content"]
+        return course_info(courseNum)
     except:
         return "ERROR"
 
     return "ERROR-NOTRY"
 
 
-@app.route("/api/chatgpt2", methods=["GET"])
+@api_bp.route("/chatgpt2", methods=["GET"])
 def call_chatgpt2():
     food = request.args.get("food")
 
@@ -137,7 +126,7 @@ def call_chatgpt2():
 
 
 # Custom CORS middleware
-@app.after_request
+@api_bp.after_request
 def add_cors_headers(response):
     # Allow requests from any origin
     response.headers["Access-Control-Allow-Origin"] = "*"
@@ -151,4 +140,6 @@ def add_cors_headers(response):
 
 
 if __name__ == "__main__":
+    app = Flask(__name__)
+    app.register_blueprint(api_bp)
     app.run(host="0.0.0.0", port=5000)
